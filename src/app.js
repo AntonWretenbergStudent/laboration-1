b4653664f927ae8beac8471b540c6c8b8ff7ef6f
Anton Wretenberg <Anton.Wretenberg@outlook.com>
2025-09-07 16:38:08 +0200

    Finished laboration-1

--- src/app.js
+++ src/app.js
@@ -1,13 +1,57 @@
-const btn = document.getElementById('btn')
-const input = document.getElementById('input')
-const order = document.getElementById('complet')
-const container = document.getElementById('container')
-const oScreen = document.getElementById('order-screen')
+const form = document.getElementById('order-form');
+const input = document.getElementById('order-input');
+const orderBtn = document.getElementById('order-btn');
 
-btn.addEventListener('click', () => {
-  order.textContent = 'hahahaahh'
-  order.style.color = 'white'
-  container.hidden = true
-  oScreen.hidden = false
+const screenOrder = document.getElementById('screen-order');
+const screenConfirm = document.getElementById('screen-confirm');
+
+const orderMsg = document.getElementById('order-msg');
+const orderValue = document.getElementById('order-value');
+
+const backBtn = document.getElementById('back-btn');
+const newOrderBtn = document.getElementById('new-order-btn');
+
+const name = document.getElementById('your-name')
+
+function capitalizeFirst(str) {
+  if (!str) return "";
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+input.addEventListener('input', () => {
+  const hasText = input.value.trim().length > 0
+  orderBtn.disabled = !hasText
+
 })
 
+function showScreen(idToShow) {
+  screenOrder.hidden = idToShow !== 'order';
+  screenConfirm.hidden = idToShow !== 'confirm';
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const value = input.value.trim()
+  const capValue = capitalizeFirst(value)
+
+  if(!value) {
+    orderMsg.textContent = 'Please enter your name'
+    return 
+  }
+  name.textContent = `Welcome ${capValue}!`
+
+  showScreen('confirm')
+})
+
+backBtn.addEventListener('click', () => {
+  showScreen('order')
+  input.focus()
+})
+
+newOrderBtn.addEventListener('click', () => {
+  showScreen('order')
+  orderMsg.textContent = ''
+  orderBtn.disable = true
+  input.value = ''
+  input.focus()
+})
